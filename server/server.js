e4a705993aa64d8565fd424ae2ddbc85380895ed
AnthonyDampier <Dampieranthony2@gmail.com>
2022-12-06 11:38:46 -0600

    API Gifs and env hidden api

--- server/server.js
+++ server/server.js
@@ -1,17 +1,38 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('build'));
 
+// !! Environment variables: HIDING API KEY
+require('dotenv').config();
+const PORT = process.env.PORT || 5000;
+// TODO: config vars on heroku to deploy app
+
+
 /** ---------- EXPRESS ROUTES ---------- **/
 
-app.use('/api', (req, res) => {
-    res.send('hello');
+// GIFFY request response
+app.get('/api/giphy', (req, res) => {
+    // TODO: make axios request
+    // TODO: include API key: api.giphy.com/v1/gifs/trending?api_key=cSbGEPhTitY9wzRAYeVlKMimJD6rTsvI
+    axios.get(
+        `http://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&rating=g&limit=1`
+    )
+    .then((response) => {
+        // TODO: console.log response
+        console.log('Got giffy: ', response);
+        res.send(response.data);
+    })
+    .catch((error) => {
+        // TODO: console.log error
+        console.log('Error on Giffy Trend GET: ',error);
+        res.sendStatus(500);
+    })
 })
 
 /** ---------- START SERVER ---------- **/
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App () {
+
+  const getThoseGifs = () => {
+    // TODO: make request to our server
+    console.log('getting gifs from our server');
+    axios.get('/api/giphy')
+    .then((response) => {
+      console.log('those gifs', response.data.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
 
     return (
       <div className="App">
@@ -9,6 +22,7 @@ function App () {
           <h1 className="App-title">APIS</h1>
           <h4><i>APIS</i></h4>
         </header>
+        <button onClick={() => getThoseGifs()}>Get Gifs</button>
       </div>
     );
   
